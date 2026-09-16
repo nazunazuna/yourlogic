c751665b74f476fdd86e0d831be46f1d63de335d
nazunazuna <jankys.no.jokes@gmail.com>
2026-09-16 14:55:01 +0900

    Load current Firebase module from js/puzzles/shikaku/shikaku.js

--- js/puzzles/shikaku/shikaku.js
+++ js/puzzles/shikaku/shikaku.js
@@ -1,5 +1,5 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { auth, fetchPuzzleById, markPuzzleFinished, saveClearRecord } from "../../services/firebaseService.js";
+import { auth, fetchPuzzleById, markPuzzleFinished, saveClearRecord } from "../../services/firebaseService.js?v=20260916-2";
 import { clearProgress, loadProgress, saveProgress } from "../../core/progressStore.js";
 import { bindUndoShortcut, createUndoHistory } from "../../core/historyStore.js";
 import { getShikakuHint } from "./shikakuHint.js";
